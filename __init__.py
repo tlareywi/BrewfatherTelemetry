e3e35345a431bc93fbda9cacc2b6a1b4b65269c3
tlareywi <trystan@trystan.org>
2019-12-26 21:08:51 -0600

    first working version

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 import logging
 import requests
+# import json
 
 from modules import cbpi
 from thread import start_new_thread
 
-headers = {'content-type': '"application/x-www-form-urlencoded; charset=utf-8"'}
+headers = {'content-type': '"application/json; charset=utf-8"'}
+unit = "F"
 
 @cbpi.initalizer(order=999)
 def init(cbpi):  # Setup BrewFather telemetry (optional)
+    global unit
+    unit = cbpi.get_config_parameter("unit", "F")
     brew_father_url = cbpi.get_config_parameter("brew_father_url", None)
     if brew_father_url is None:
         cbpi.add_config_parameter("brew_father_url", "", "text", "Brewfather.app custom stream url; i.e. http://log.brewfather.net/stream?id=xxxxxxxxxxxxxx")
@@ -22,25 +26,22 @@ def brewfather_telemetry_task(api):
 
     brew_name = cbpi.get_config_parameter("brew_name", None)
 
-    now = datetime.datetime.now()
     for key, value in cbpi.cache.get("sensors").iteritems():
-        cbpi.app.logger.info(value)
-        if (value.instance.type == "ONE_WIRE_SENSOR"):
+        if (value.type == "ONE_WIRE_SENSOR"):
             data = {}
-            data['name'] = value.instance.name
-            data['temp'] = value.instance.value
-            data['aux_temp'] = 0
-            data['ext_temp'] = 0
-            data['temp_unit'] = value.instance.unit
+            data['name'] = value.name
+            data['temp'] = value.instance.last_value
+            data['aux_temp'] = 0.0
+            data['ext_temp'] = 0.0
+            data['temp_unit'] = unit
             data['gravity'] = 0.000
             data['gravity_unit'] = "G" #SpGr
             data['pressure'] = 0
             data['pressure_unit'] = "PSI"
-            data['ph'] = 0
-            data['comment'] = ""
+            data['ph'] = 0.0
+            data['comment'] = "BrewfatherTelemetry"
             data['beer'] = brew_name
-            r = requests.post(url, headers=headers, data=data)
-            cbpi.app.logger.info("Result %s" % r.text)
-
-
+            # cbpi.app.logger.info("brewfather_telemetry: JSON %s" % json.dumps(data))
+            r = requests.post(brew_father_url, json=data)
+            cbpi.app.logger.info("brewfather_telemetry: Result %s" % r.text)
 
